745058c91733dc9fc9c4f3c6708f80baf9c74e68
Gorgohn <alisanyildiz0930@icloud.com>
2026-06-21 12:10:55 +0200

    changed exists() to is_dir() for better control

--- backup.py
+++ backup.py
@@ -1,6 +1,6 @@
 import datetime 
 import pathlib
-
+import shutil
 
 source_icloud = pathlib.Path(r"C:/Users/amd/iCloudDrive")
 backup_root = pathlib.Path(r"E:/")
@@ -11,7 +11,7 @@ month = today.month
 
 target = backup_root / str(year) / str(month)
 
-if source_icloud.exists():
+if source_icloud.is_dir():
     print("Quelle gefunden")
     target.mkdir(parents=True, exist_ok=True)
     print(target)
