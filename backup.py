ca0d9a1fe77d18220670bcccef27057545e6fabe
Gorgohn <alisanyildiz0930@icloud.com>
2026-06-26 22:50:23 +0200

    Changed the appearance of the output

--- backup.py
+++ backup.py
@@ -14,9 +14,10 @@ def get_time():
     today = datetime.date.today()
     year = today.year
     month = today.month
-    return today, year, month
+    day = today.day
+    return today, year, month, day
 
-today, year, month = get_time()
+today, year, month, day = get_time()
 
 def set_target():
     target = backup_root / str(year) / str(month)
@@ -24,21 +25,29 @@ def set_target():
 
 target = set_target()
 
+def set_logging_dir():
+    logging_dir = backup_root / "Logging data"
+    logging_dir.mkdir(parents=True, exist_ok=True)
+    return logging_dir
+
+logging_dir = set_logging_dir()
+
 def check_is_dir():
     if not source_icloud.is_dir(): # searching for icloud
         print("Icloud not found")
         sys.exit(1)
+    else:
+        print("Icloud found")
 
     if not backup_root.is_dir(): # searching for backup
         print("Backup not found")
         sys.exit(1)
+    else:
+        print("Backup found")
 
 check_is_dir()
 
-print("Icloud found")
-print("Backup found")
-
-def run_backup(): # creating function
+def run_backup(): # backup process 
     copied_files = 0
     skipped_files = 0
     updated_files = 0
@@ -71,6 +80,16 @@ def run_backup(): # creating function
                     print(f"Error while copying file: {new_file}")
     return copied_files, updated_files, skipped_files, failed_update_files, failed_copy_files # returning values to global
 
-copied_files, updated_files, skipped_files, failed_update_files, failed_copy_files = run_backup() # getting values and running function
+copied_files, updated_files, skipped_files, failed_update_files, failed_copy_files = run_backup()
 
-print(f"Backup finished. Copied files: {copied_files}. Updated files: {updated_files}. Skipped files: {skipped_files}. Failed update files: {failed_update_files}. Failed copy files: {failed_copy_files}.")
+def create_logging_file():
+    backup_log = logging_dir / f"Backup_log_{year}_{month}_{day}.txt"
+    backup_log = open(backup_log, "w")
+    backup_log.write(f"Backup finished.\nCopied files: {copied_files}.\nUpdated files: {updated_files}.\nSkipped files: {skipped_files}.\nFailed update files: {failed_update_files}.\nFailed copy files: {failed_copy_files}.\nDate: {year}.{month}.{day}")
+    backup_log.close
+    print("Backup log created")
+    return backup_log
+
+backup_log = create_logging_file()
+
+print(f"Backup finished.\nCopied files: {copied_files}.\nUpdated files: {updated_files}.\nSkipped files: {skipped_files}.\nFailed update files: {failed_update_files}.\nFailed copy files: {failed_copy_files}.\nDate: {year}.{month}.{day}")
